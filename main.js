3250dff9c0d69c0626f1f7b5025c6bcaa4568508
Marissa Fullford <31824846+mfullford@users.noreply.github.com>
2017-09-17 22:05:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	},
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+};
+
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+var createBoard = function(){
+	 for (var i = 0; i < cards.length; i++){
+	 	var cardElement = document.createElement('img');
+	 	cardElement.setAttribute("src", "images/back.png");
+	 	cardElement.setAttribute('data-id', i);
+	 	cardElement.addEventListener('click', flipCard);
+	 	var board = document.getElementById("game-board");
+		board.appendChild(cardElement);
+	 }
+}
+
+createBoard();
